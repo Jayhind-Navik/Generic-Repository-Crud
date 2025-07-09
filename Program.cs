29d6e8126bf4af554128a1855a64776f41d1c799
Jayhind Navik <125726357+Jayhind-Navik@users.noreply.github.com>
2025-07-09 09:50:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Using_GenericRepository_CrudOperation.Data;
+using Using_GenericRepository_CrudOperation.GenericSerivce;
+using Using_GenericRepository_CrudOperation.Models;
+using Using_GenericRepository_CrudOperation.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+//builder.Services.AddTransient<IGenericRepository<tblEmployee>, GenericRepository<tblEmployee>>();
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IGenericService,GenericService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
